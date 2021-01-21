376bfb2274233f6d2b59a2a3b21639f92367efff
Vash <vash_dn@mail.ru>
2021-01-21 09:40:26 +0300

    Добавил установку по кнопке, сервис воркер, и кеширование информации

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -96,7 +96,7 @@ const About: React.FC<TAbout> = observer(
 					0,
 					document.querySelector(".App")?.clientHeight! -
 						scrollRef.current!.clientHeight -
-						254,
+						document.querySelector("footer")?.clientHeight!,
 				)
 			} else {
 				window.scrollTo(0, 0)
